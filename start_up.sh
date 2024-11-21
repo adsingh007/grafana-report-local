c333e67c253e0b725933a0a8d0effff0c11eb3ad
Amandeep Singh <amandeep.singh@fosteringlinux.com>
2024-11-21 17:30:24 +0530

    added script

---
+++ start_up.sh
@@ -0,0 +1,62 @@
+############Install node-exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+tar xzf node_exporter-1.8.2.linux-amd64.tar.gz
+#Remove node_exporter-1.7.0.linux-amd64.tar.gz
+rm -rf node_exporter-1.8.2.linux-amd64.tar.gz
+
+mv node_exporter-1.8.2.linux-amd64 /etc/node_exporter
+
+sudo tee /etc/systemd/system/node_exporter.service > /dev/null <<EOF
+[Unit]
+Description=Node Exporter
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/etc/node_exporter/node_exporter
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl restart node_exporter
+
+sudo systemctl status node_exporter
+
+#############
+
+mkdir -p grafana/data
+mkdir -p prometheus/data
+mkdir -p prometheus/config
+
+tee prometheus/config/prometheus.yml > /dev/null <<EOF
+global:
+  scrape_interval: 15s
+
+scrape_configs:
+- job_name: node
+  static_configs:
+  - targets: ['localhost:9100']
+EOF
+
+
+chmod -R 777 prometheus grafana
+
+##############Start grafana-stack
+podman load -q -i grafana-report.tar 
+podman run pod --name grafana-stack -p 3000:3000 -p 8686:8686 -p 8081:8081
+podman run -d --name=grafana --pod grafana-stack -v $(pwd)/grafana/data:/var/lib/grafana  grafana/grafana
+podman run -d --name=grafana-image-renderer --pod grafana-stack grafana/grafana-image-renderer:latest
+podman run -d --name=grafana-report --pod grafana-stack localhost/grafana-report:v1 
+
+#############Start Prometheus
+
+podman run -d --name=prometheus \
+  -p 9090:9090 \
+  -v $(pwd)/prometheus/data:/prometheus \
+  -v $(pwd)/prometheus/config/prometheus.yml:/etc/prometheus/prometheus.yml:ro \
+  prom/prometheus
+
